0e6837dbcef98a0058254c55aa65d92dce554e02
MarachLJLL <marachljll@gmail.com>
2024-01-27 14:43:18 -0500

    send/accept drafting

---
+++ AccepterAPI/accepter.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+'''
+@app.route("/")
+def home():
+    return "Home"
+'''
+
+@app.route("/get-user/<uid>")
+def get_user(uid):
+    user_data = {
+        "uid": uid,
+        "name": "John",
+        "email": "John Cena"
+    }
+
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+    return jsonify(user_data), 201
+
+@app.route("/login/", methods=["GET"])
+def login():
+    return True
+
+@app.route("/create-dp", methods=["POST"])
+def create_dp():
+    # if request.method == "POST":
+    data = request.get_json()
+        
+    # implement adding the data into the database
+    print(data)
+    
+    return jsonify(data), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
