ea6c269d91a2376b875bf8911a31fd1efecc3a69
IHu04 <isaachu2004@gmail.com>
2024-01-27 17:54:13 -0500

    t

--- ViewerApp/static/classes.py
+++ ViewerApp/static/classes.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from sqlalchemy.dialects.postgresql import ARRAY
 
 db = SQLAlchemy()
 
@@ -8,14 +7,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     password = db.Column(db.String(100))
-    devices = db.Column(db.JSON)
 
-class Device(db.Model):
+class DP(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     device_id = db.Column(db.String(100))
-    expenditure_entries = db.Column(db.ExpenditureEntry(1000))
-
-class ExpenditureEntry(db.Model):
-    time = db.Column(db.datetime(100))
-    energy = db.Column(db.Float(100))
+    time = db.Column(db.DateTime)
+    energy = db.Column(db.Float)
 
 
