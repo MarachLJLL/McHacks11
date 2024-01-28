799989f0dc858407a7c608d37fdbd96c345be3bb
MarachLJLL <marachljll@gmail.com>
2024-01-27 19:28:35 -0500

    update

--- ViewerApp/static/graphing.py
+++ ViewerApp/static/graphing.py
@@ -1,29 +1,6 @@
 from datetime import datetime, timedelta
 import plotly.graph_objects as go
 
-def getDevices(user):
-    devicesList = []
-    # get all entries for this user
-    # have a Counter() for all of the devices
-    # set devicesList to the keys of the Counter()
-    return devicesList
-
-def convertTimeToDatetime(str):
-    year = 1
-    month = 1
-    day =  1
-    hour = 1
-    minute = 1
-    return datetime(year=year, month=month, day=day, hour=hour, minute=minute)
-
-def getTimes(device):
-    times_list = []
-    return times_list
-
-def getEnergies(device):
-    energies_list = []
-    return energies_list
-
 def getGraphHtml(user):
     devices = getDeviceIDs(user)
     times_lists = []
